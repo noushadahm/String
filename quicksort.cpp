113cec6fd924b0e27c57d8dc204e872d644ec449
Noushad Ahmed <ndahmad6000@gmail.com>
2021-02-15 08:37:09 +0530

    initial commit

---
+++ quicksort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void swap(int arr[],int i, int j ){
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;	
+}
+int partition(int ar[], int low, int high){
+    int pivot=ar[high];
+    int i=low-1;
+    for(int j=low; j<high; j++){
+      if(ar[j]<=pivot){
+      	i++;
+        swap(ar,i,j);	
+      }	
+    }
+    swap(ar,i+1,high);
+    return i+1;	
+}
+
+
+void quicksort(int arr[], int low, int high){
+      if(low<high){
+      
+      int pi=partition(arr, low ,high);
+      quicksort(arr, low, pi-1);
+      quicksort(arr, pi+1, high);
+}
+}
+int main(){
+	
+   int arr[]={5,4,3,2,1};
+   quicksort(arr,0,4);
+   for(int i=0; i<5; i++){
+    cout<<arr[i]<<" ";	
+   }
+   return 0;
+}
